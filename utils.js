cf6b57caae842118007af933f7831937e6f19cd0
saeedgoraya987 <saeedgoraya987@gmail.com>
2025-12-25 08:44:19 +0500

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,9 +1,11 @@
 export const PHONE_RE = /^\+?\d{8,18}$/;
 
-export const normalizeNumber = (raw) => {
+export function normalizeNumber(raw) {
   if (!raw) return null;
   const s = String(raw).replace(/[^\d+]/g, "");
   return PHONE_RE.test(s) ? s : null;
-};
+}
 
-export const toJid = (n) => n.replace(/\D/g, "") + "@s.whatsapp.net";
+export function toJid(n) {
+  return n.replace(/\D/g, "") + "@s.whatsapp.net";
+}
