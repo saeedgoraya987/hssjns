8154bb4790710c91d8d5de4e2472d33c58bfa8e6
saeedgoraya987 <saeedgoraya987@gmail.com>
2026-02-19 15:46:26 +0500

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,2 @@
-import makeWASocket from '@itsukichan/baileys'
-
-const suki = makeWASocket({
-   printQRInTerminal: false
-})
-
-if (!suki.authState.creds.registered) {
-   const number = '923091731496'
-   const code = await suki.requestPairingCode(number)
-   console.log(code)
-}
+import * as test from "@itsukichan/baileys";
+console.log(test);
